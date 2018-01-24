353ee657b616a766e4a9f96bc8a1864e76bd2695
Yurec865 <shnurenko.yuriy@gmail.com>
2018-01-25 00:21:22 +0300

    renewal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,12 @@ public class Main {
     public static void main(String[] args) {
         MultiStoreyBuilding multiStoreyBuilding = new MultiStoreyBuilding();
         OrdinaryHouse ordinaryHouse = new OrdinaryHouse();
+        multiStoreyBuilding.outputNumber();
 
         System.out.println("Square ordinaryHouse: " + ordinaryHouse.getSquare());
         System.out.println("Is ordinal ordinaryHouse: " + ordinaryHouse.isOrdinal());
         System.out.println("Square multiStoreyBuilding: " +multiStoreyBuilding.getSquare());
         System.out.println("Is ordinal multiStoreyBuilding: " +multiStoreyBuilding.isOrdinal());
+
     }
 }
